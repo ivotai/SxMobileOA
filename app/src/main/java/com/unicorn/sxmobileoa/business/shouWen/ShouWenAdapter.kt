278970ec76d2074894e8a6c34319ba86939656c7
ivotai <ivotai@163.com>
2018-08-19 11:28:36 +0800

    收文 item 布局

--- app/src/main/java/com/unicorn/sxmobileoa/business/shouWen/ShouWenAdapter.kt
+++ app/src/main/java/com/unicorn/sxmobileoa/business/shouWen/ShouWenAdapter.kt
@@ -3,11 +3,19 @@ package com.unicorn.sxmobileoa.business.shouWen
 import com.chad.library.adapter.base.BaseQuickAdapter
 import com.chad.library.adapter.base.BaseViewHolder
 import com.unicorn.sxmobileoa.R
+import com.unicorn.sxmobileoa.app.chongqing.Key
+import org.joda.time.DateTime
 
-class ShouWenAdapter: BaseQuickAdapter<ShouWen, BaseViewHolder>(R.layout.item_shou_wen) {
+class ShouWenAdapter : BaseQuickAdapter<ShouWen, BaseViewHolder>(R.layout.item_shou_wen) {
 
     override fun convert(helper: BaseViewHolder, item: ShouWen) {
-        helper.setText(R.id.tv,item.bt)
+        item.apply {
+            helper.setText(R.id.tvBt, bt)
+            helper.setText(R.id.tvWh, wh)
+            helper.setText(R.id.tvNgr, ngr)
+            helper.setText(R.id.tvNgbm, ngbm)
+            helper.setText(R.id.tvSj, DateTime(sj).toString(Key.DATE_VALUE_FORMAT))
+        }
     }
 
 }
