6d566d6af23374361d84b1f34b6414ffbee31fa0
ivotai <ivotai@163.com>
2018-09-05 20:44:10 +0800

    shadow

---
+++ app/src/main/java/com/unicorn/sxmobileoa/login/parse/parameter.java
@@ -0,0 +1,14 @@
+package com.unicorn.sxmobileoa.login.parse;
+
+import org.simpleframework.xml.Attribute;
+import org.simpleframework.xml.Text;
+
+public class parameter {
+
+    @Attribute
+    public String name;
+
+    @Text
+    public String text ;
+
+}
