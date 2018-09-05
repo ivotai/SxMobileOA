089072cf4afcb58152e2b15010334b586259a043
ivotai <ivotai@163.com>
2018-09-05 20:43:55 +0800

    shadow

--- app/src/main/java/com/unicorn/sxmobileoa/login/parse/Parameters.java
+++ app/src/main/java/com/unicorn/sxmobileoa/login/parse/Parameters.java
@@ -1,14 +1,12 @@
 package com.unicorn.sxmobileoa.login.parse;
 
 import org.simpleframework.xml.ElementList;
-import org.simpleframework.xml.Root;
 
 import java.util.List;
 
-@Root(name = "parameters")
-public class Parameters {
+public class parameters {
 
-    @ElementList(entry = "parameter",inline = true,required = false)
-    public List<Parameter> parameters;
+    @ElementList(entry = "parameter",inline = true)
+    public List<parameter> parameterList;
 
 }
