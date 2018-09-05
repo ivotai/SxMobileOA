983afd3c33a1a4e6182a584e8ac84ebea0ae4d02
ivotai <ivotai@163.com>
2018-09-05 20:43:50 +0800

    shadow

--- app/src/main/java/com/unicorn/sxmobileoa/login/parse/Response.java
+++ app/src/main/java/com/unicorn/sxmobileoa/login/parse/Response.java
@@ -1,108 +1,18 @@
 package com.unicorn.sxmobileoa.login.parse;
 
+import org.simpleframework.xml.Element;
+import org.simpleframework.xml.Root;
+
+@Root(strict = false)
 public class Response {
 
-    private String code;
-    private String msg;
-    private String randCode;
-    private String randCodeSec;
-    private String loginBusiType;
-    private String time;
-    private String seqM;
-    private String secM;
-    private String seqD;
-    private String seqR;
+    @Element
+    public String code;
 
-    private Parameters parameters;
+    @Element
+    public String msg;
 
-    //
-
-    public Parameters getParameters() {
-        return parameters;
-    }
-
-    public void setParameters(Parameters parameters) {
-        this.parameters = parameters;
-    }
-
-    public String getCode() {
-        return code;
-    }
-
-    public void setCode(String code) {
-        this.code = code;
-    }
-
-    public String getMsg() {
-        return msg;
-    }
-
-    public void setMsg(String msg) {
-        this.msg = msg;
-    }
-
-    public String getRandCode() {
-        return randCode;
-    }
-
-    public void setRandCode(String randCode) {
-        this.randCode = randCode;
-    }
-
-    public String getRandCodeSec() {
-        return randCodeSec;
-    }
-
-    public void setRandCodeSec(String randCodeSec) {
-        this.randCodeSec = randCodeSec;
-    }
-
-    public String getLoginBusiType() {
-        return loginBusiType;
-    }
-
-    public void setLoginBusiType(String loginBusiType) {
-        this.loginBusiType = loginBusiType;
-    }
-
-    public String getTime() {
-        return time;
-    }
-
-    public void setTime(String time) {
-        this.time = time;
-    }
-
-    public String getSeqM() {
-        return seqM;
-    }
-
-    public void setSeqM(String seqM) {
-        this.seqM = seqM;
-    }
-
-    public String getSecM() {
-        return secM;
-    }
-
-    public void setSecM(String secM) {
-        this.secM = secM;
-    }
-
-    public String getSeqD() {
-        return seqD;
-    }
-
-    public void setSeqD(String seqD) {
-        this.seqD = seqD;
-    }
-
-    public String getSeqR() {
-        return seqR;
-    }
-
-    public void setSeqR(String seqR) {
-        this.seqR = seqR;
-    }
+    @Element(required = false)
+    public parameters parameters;
 
 }
--- app/src/main/java/com/unicorn/sxmobileoa/login/useCase/SimpleResponse.kt
+++ app/src/main/java/com/unicorn/sxmobileoa/login/useCase/SimpleResponse.kt
@@ -1,11 +1,9 @@
 package com.unicorn.sxmobileoa.login.useCase
 
-class SimpleResponse<Model>(
+data class SimpleResponse<Model>(
         val code: String,
         val msg: String
 ) {
-
     var message: String? = null
     var result: Model? = null
-
 }
