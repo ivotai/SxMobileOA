48c7c6da28cc6e06ebfd009f4322ece3bd5f1e79
ivotai <ivotai@163.com>
2018-09-06 15:28:00 +0800

    优化代码

--- app/src/main/java/com/unicorn/sxmobileoa/app/union/RandomGeneter.java
+++ app/src/main/java/com/unicorn/sxmobileoa/app/union/RandomGeneter.java
@@ -24,21 +24,21 @@ public class RandomGeneter {
 		return stringBuffer.toString();
 	}
 
-	/**
-	 * * 返回一个定长的随机纯字母字符串(只包含大小写字母) * *
-	 * 
-	 * @param length *
-	 *            随机字符串长度 *
-	 * @return 随机字符串
-	 */
-	private static String generateMixString(int length) {
-		StringBuilder stringBuffer = new StringBuilder();
-		Random random = new Random(System.currentTimeMillis());
-		for (int i = 0; i < length; i++) {
-			stringBuffer.append(allChar.charAt(random.nextInt(letterChar.length())));
-		}
-		return stringBuffer.toString();
-	}
+//	/**
+//	 * * 返回一个定长的随机纯字母字符串(只包含大小写字母) * *
+//	 *
+//	 * @param length *
+//	 *            随机字符串长度 *
+//	 * @return 随机字符串
+//	 */
+//	private static String generateMixString(int length) {
+//		StringBuilder stringBuffer = new StringBuilder();
+//		Random random = new Random(System.currentTimeMillis());
+//		for (int i = 0; i < length; i++) {
+//			stringBuffer.append(allChar.charAt(random.nextInt(letterChar.length())));
+//		}
+//		return stringBuffer.toString();
+//	}
 
 
 //	public static String generateLowerString(int length) {
@@ -50,20 +50,20 @@ public class RandomGeneter {
 //		return generateMixString(length).toUpperCase();
 //	}
 
-	/**
-	 * * 生成一个定长的纯0字符串 * *
-	 * 
-	 * @param length *
-	 *            字符串长度 *
-	 * @return 纯0字符串
-	 */
-	private static String generateZeroString(int length) {
-		StringBuilder stringBuffer = new StringBuilder();
-		for (int i = 0; i < length; i++) {
-			stringBuffer.append('0');
-		}
-		return stringBuffer.toString();
-	}
+//	/**
+//	 * * 生成一个定长的纯0字符串 * *
+//	 *
+//	 * @param length *
+//	 *            字符串长度 *
+//	 * @return 纯0字符串
+//	 */
+//	private static String generateZeroString(int length) {
+//		StringBuilder stringBuffer = new StringBuilder();
+//		for (int i = 0; i < length; i++) {
+//			stringBuffer.append('0');
+//		}
+//		return stringBuffer.toString();
+//	}
 
 
 //	public static String toFixdLengthString(long num, int fixdlenth) {
@@ -92,17 +92,5 @@ public class RandomGeneter {
 //		sb.append(strNum);
 //		return sb.toString();
 //	}
-
-	public static void main(String[] args) {
-		System.out.println(generateString(6));
-		// System.out.println(generateMixString(6));
-		// //System.out.println(generateLowerString(6));
-		// //System.out.println(generateUpperString(6));
-		// //System.out.println(generateZeroString(6));
-		// //System.out.println(toFixdLengthString(123,
-		// 15));
-		// //System.out.println(toFixdLengthString(123L,
-		// 15)); }
-	}
 
 }
