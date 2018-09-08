ddacc12471bb3893560dd12ae3a0749f98ec310e
ivotai <ivotai@163.com>
2018-09-08 14:06:13 +0800

    展示Tab

--- app/src/main/java/com/unicorn/sxmobileoa/main/MainAct.kt
+++ app/src/main/java/com/unicorn/sxmobileoa/main/MainAct.kt
@@ -1,5 +1,6 @@
 package com.unicorn.sxmobileoa.main
 
+import android.support.design.widget.TabLayout.MODE_SCROLLABLE
 import android.support.v7.widget.GridLayoutManager
 import com.unicorn.sxmobileoa.R
 import com.unicorn.sxmobileoa.app.Global
@@ -7,7 +8,10 @@ import com.unicorn.sxmobileoa.app.ui.BaseAct
 import com.unicorn.sxmobileoa.main.data.Faker
 import com.unicorn.sxmobileoa.main.ui.HeaderView
 import com.unicorn.sxmobileoa.main.ui.MainAdapter
+import io.reactivex.Observable
 import kotlinx.android.synthetic.main.act_main.*
+
+
 
 class MainAct : BaseAct() {
 
@@ -15,8 +19,39 @@ class MainAct : BaseAct() {
 
     override fun initViews() {
         titleBar.setTitle(Global.court!!.dmms)
+        intSlidingTabLayout()
         initRecyclerView()
     }
+
+    private fun intSlidingTabLayout() {
+        tabLayout.tabMode = MODE_SCROLLABLE
+
+        Faker().getMainSection()
+                .toObservable()
+                .flatMap { Observable.fromIterable(it) }
+                .filter { it.isHeader }
+                .map { it.header }
+                .subscribe{ it->
+                    val tab = tabLayout.newTab()
+                    tab.setText(it)
+                    tabLayout.addTab(tab)
+
+                }
+//        val list =ArrayList<String>()
+//        tab.setTitles(arrayOf("",""))
+//
+//        addNumbers(*intArrayOf(1,2))
+    }
+
+
+    fun addNumbers( vararg args: Int): Int {
+        var result = 0
+        for (i in args.indices) {
+            result += args[i]
+        }
+        return result
+    }
+
 
     private val mainAdapter = MainAdapter()
 
