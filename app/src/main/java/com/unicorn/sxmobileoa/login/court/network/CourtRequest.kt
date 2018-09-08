32657b43545f24b82ef5a0a1086409840adc44d9
ivotai <ivotai@163.com>
2018-09-08 09:49:42 +0800

    测试混淆编译

--- app/src/main/java/com/unicorn/sxmobileoa/login/court/network/CourtRequest.kt
+++ app/src/main/java/com/unicorn/sxmobileoa/login/court/network/CourtRequest.kt
@@ -2,7 +2,6 @@ package com.unicorn.sxmobileoa.login.court.network
 
 import com.unicorn.sxmobileoa.app.network.model.MaybeRequest
 
-//@Root(name = "request")
 class CourtRequest : MaybeRequest(busiCode = "fyxx") {
 
     init {
--- app/src/main/java/com/unicorn/sxmobileoa/main/MainAct.kt
+++ app/src/main/java/com/unicorn/sxmobileoa/main/MainAct.kt
@@ -1,22 +1,18 @@
 package com.unicorn.sxmobileoa.main
 
-import android.content.Intent
 import com.unicorn.sxmobileoa.R
-import com.unicorn.sxmobileoa.app.safeClicks
 import com.unicorn.sxmobileoa.app.ui.BaseAct
-import com.unicorn.sxmobileoa.dblb.ui.DblbAct
 import kotlinx.android.synthetic.main.act_bgsx.*
 
 class MainAct : BaseAct() {
 
-    override val layoutId = R.layout.act_bgsx
+    override val layoutId = R.layout.act_main
 
     override fun initViews() {
-        titleBar.setTitle("办公事项")
+        titleBar.setTitle("陕西省高级人民法院移动办公")
     }
 
     override fun bindIntent() {
-        jddb.safeClicks().subscribe { startActivity(Intent(this, DblbAct::class.java)) }
     }
 
 }
