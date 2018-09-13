ca1518f99a1ef3f511462da18c2a5984075c5a97
ivotai <ivotai@163.com>
2018-09-13 10:00:50 +0800

    user ok

--- app/src/main/java/com/unicorn/sxmobileoa/spdNext/ui/UserAdapter.kt
+++ app/src/main/java/com/unicorn/sxmobileoa/spdNext/ui/UserAdapter.kt
@@ -1,15 +1,30 @@
 package com.unicorn.sxmobileoa.spdNext.ui
 
+import android.graphics.Color
+import android.support.v4.content.ContextCompat
+import android.widget.TextView
 import com.chad.library.adapter.base.BaseQuickAdapter
 import com.chad.library.adapter.base.BaseViewHolder
 import com.unicorn.sxmobileoa.R
 import com.unicorn.sxmobileoa.app.mess.SelectWrapper
+import com.unicorn.sxmobileoa.app.safeClicks
 import com.unicorn.sxmobileoa.spdNext.model.User
 
 class UserAdapter : BaseQuickAdapter<SelectWrapper<User>, BaseViewHolder>(R.layout.item_text) {
 
     override fun convert(helper: BaseViewHolder, item: SelectWrapper<User>) {
-        helper.setText(R.id.tvText, item.t.userFullName)
+        val tvText = helper.getView<TextView>(R.id.tvText)
+        tvText.text = item.t.userFullName
+
+        // 选中效果
+        tvText.setTextColor(if (item.isSelected) Color.WHITE else Color.BLACK)
+        tvText.setBackgroundColor(if (item.isSelected) ContextCompat.getColor(mContext, R.color.colorPrimary) else Color.WHITE)
+
+        // 点击后刷新对应条目
+        tvText.safeClicks().subscribe {
+            item.isSelected = !item.isSelected
+            notifyItemChanged(helper.adapterPosition)
+        }
     }
 
 }
