de8fd110513b3ced5c4d2bf5200d8d53b202a3fb
ivotai <ivotai@163.com>
2018-09-13 20:23:14 +0800

    优化代码

--- app/src/main/java/com/unicorn/sxmobileoa/simple/dept/model/DeptResult.kt
+++ app/src/main/java/com/unicorn/sxmobileoa/simple/dept/model/DeptResult.kt
@@ -1,4 +1,8 @@
 package com.unicorn.sxmobileoa.simple.dept.model
 
-// key 代表哪个TextView发起的部门选择
-class DeptResult(val key: String, val result: String)
+// key表示由哪个textView发起的部门选择
+// result表示选择结果，由，号分割
+data class DeptResult(
+        val key: String,
+        val result: String
+)
--- app/src/main/java/com/unicorn/sxmobileoa/simple/dept/ui/DeptAct.kt
+++ app/src/main/java/com/unicorn/sxmobileoa/simple/dept/ui/DeptAct.kt
@@ -1,27 +1,21 @@
 package com.unicorn.sxmobileoa.simple.dept.ui
 
 import android.support.v7.widget.LinearLayoutManager
-import com.jakewharton.rxbinding2.widget.RxTextView
 import com.unicorn.sxmobileoa.R
 import com.unicorn.sxmobileoa.app.addDefaultItemDecoration
 import com.unicorn.sxmobileoa.app.mess.KeywordHeaderView
 import com.unicorn.sxmobileoa.app.mess.RxBus
 import com.unicorn.sxmobileoa.app.mess.SelectWrapper
 import com.unicorn.sxmobileoa.app.safeClicks
+import com.unicorn.sxmobileoa.app.textChanges
 import com.unicorn.sxmobileoa.app.ui.BaseAct
 import com.unicorn.sxmobileoa.simple.dept.model.Dept
 import com.unicorn.sxmobileoa.simple.dept.model.DeptResult
 import com.unicorn.sxmobileoa.simple.dept.network.GetDept
 import dart.DartModel
-import io.reactivex.Observable
 import kotlinx.android.synthetic.main.act_title_recycler.*
 
 class DeptAct : BaseAct() {
-
-    @DartModel
-     lateinit var model: DeptActNavigationModel
-
-    override val layoutId = R.layout.act_title_recycler
 
     override fun initViews() {
         titleBar.setTitle("选择部门")
@@ -36,32 +30,30 @@ class DeptAct : BaseAct() {
             deptAdapter.bindToRecyclerView(this)
             addDefaultItemDecoration()
         }
-        addHeaderView()
+        addKeywordHeaderView()
     }
 
     private lateinit var headerView: KeywordHeaderView
 
-    private fun addHeaderView() {
+    private fun addKeywordHeaderView() {
         headerView = KeywordHeaderView(this)
         headerView.setHint("请输入部门")
         deptAdapter.addHeaderView(headerView)
     }
 
     override fun bindIntent() {
-        GetDept().toMaybe(this).toObservable()
-                .flatMap { Observable.fromIterable(it.deptData) }
-                // 过滤父节点
-                .filter { it.value.length != 1 }
-                .toList()
-                .doOnSuccess { observeKeyword(it) }
-                .map { deptList ->
-                    ArrayList<SelectWrapper<Dept>>().apply {
-                        deptList.forEach { dept -> this.add(SelectWrapper(dept)) }
-                    }
-                }
-                .subscribe { t -> deptAdapter.setNewData(t) }
-
+        getDept()
         clickConfirm()
+    }
+
+    private fun getDept() {
+        GetDept().toMaybe(this)
+                .map { it.deptData }
+                // 过滤父节点
+                .map { it.filter { dept -> dept.value.length != 1 } }
+                .doOnSuccess { observeKeyword(it) }
+                .map { it.map { dept -> SelectWrapper(dept) } }
+                .subscribe { t -> deptAdapter.setNewData(t) }
     }
 
     private fun clickConfirm() {
@@ -69,8 +61,8 @@ class DeptAct : BaseAct() {
             deptAdapter.data
                     .filter { wrapper -> wrapper.isSelected }
                     .map { wrapperSelected -> wrapperSelected.t }
-                    .let { deptSelected ->
-                        val result = deptSelected.joinToString(",") { dept -> dept.text }
+                    .let { deptListSelected ->
+                        val result = deptListSelected.joinToString(",") { dept -> dept.text }
                         RxBus.get().post(DeptResult(model.key, result))
                     }
             finish()
@@ -78,13 +70,17 @@ class DeptAct : BaseAct() {
     }
 
     private fun observeKeyword(deptList: List<Dept>) {
-        RxTextView.textChanges(headerView.etKeyword)
-                .map { it.toString() }
+        headerView.etKeyword.textChanges()
                 .subscribe { keyword ->
                     deptList.filter { dept -> dept.text.contains(keyword) }
                             .map { dept -> SelectWrapper(dept) }
                             .let { deptAdapter.setNewData(it) }
                 }
     }
+
+    @DartModel
+    lateinit var model: DeptActNavigationModel
+
+    override val layoutId = R.layout.act_title_recycler
 
 }
