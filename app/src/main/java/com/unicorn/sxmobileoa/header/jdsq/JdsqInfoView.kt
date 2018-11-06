48a65037206d3b1260e70aba7401cefe4e00cdfc
ivotai <ivotai@163.com>
2018-11-06 15:49:49 +0800

    重构接待

--- app/src/main/java/com/unicorn/sxmobileoa/header/jdsq/JdsqInfoView.kt
+++ app/src/main/java/com/unicorn/sxmobileoa/header/jdsq/JdsqInfoView.kt
@@ -1,74 +1,41 @@
 package com.unicorn.sxmobileoa.header.jdsq
 
 import android.annotation.SuppressLint
-import android.arch.lifecycle.LifecycleOwner
 import android.content.Context
 import android.view.LayoutInflater
+import android.view.View
 import android.widget.FrameLayout
 import android.widget.TextView
 import com.unicorn.sxmobileoa.R
-import com.unicorn.sxmobileoa.app.*
-import com.unicorn.sxmobileoa.app.mess.RxBus
-import com.unicorn.sxmobileoa.app.mess.SpdHelper
-import com.unicorn.sxmobileoa.app.mess.model.TextResult
+import com.unicorn.sxmobileoa.app.Global
+import com.unicorn.sxmobileoa.app.Key
+import com.unicorn.sxmobileoa.app.get
 import com.unicorn.sxmobileoa.header.BasicInfoView
 import com.unicorn.sxmobileoa.header.PAIR
 import com.unicorn.sxmobileoa.simple.main.model.Menu
 import com.unicorn.sxmobileoa.spd.model.Spd
-import io.reactivex.functions.Consumer
+import kotlinx.android.extensions.LayoutContainer
+import kotlinx.android.synthetic.main.header_view_jdsq.view.*
 
 @SuppressLint("ViewConstructor")
-class JdsqInfoView(context: Context, menu: Menu, spd: Spd) : FrameLayout(context), BasicInfoView {
+class JdsqInfoView(context: Context, menu: Menu, spd: Spd,isCreate:Boolean) : FrameLayout(context), BasicInfoView , LayoutContainer {
 
-    init {
-        initViews(context, menu, spd)
-    }
-
-    lateinit var tvTitle: TextView
-    lateinit var tvBt: TextView
-    lateinit var tvSqr: TextView
-    lateinit var tvSqrq: TextView
-    lateinit var tvCbbm: TextView
-    lateinit var tvJcdd: TextView
-    lateinit var tvCbr: TextView
-    lateinit var tvJdrq: TextView
-    lateinit var tvLfkrdw: TextView
-    lateinit var tvXm: TextView
-    lateinit var tvLfrs: TextView
-    lateinit var tvJb: TextView
-    lateinit var tvPtry: TextView
-    lateinit var tvLfsy: TextView
-    lateinit var tvJdje: TextView
-    lateinit var tvBz: TextView
+    override val containerView = this
 
     private lateinit var pairs: ArrayList<PAIR<TextView, String>>
 
-    fun initViews(context: Context, menu: Menu, spd: Spd) {
-        LayoutInflater.from(context).inflate(R.layout.header_view_jdsq, this, true)
-        findView()
-        renderView(menu, spd)
-        canEdit(spd)
+    init {
+        initViews(context, menu, spd,isCreate)
     }
 
-    private fun findView() {
-        tvTitle = findViewById(R.id.tvTitle)
-        tvBt = findViewById(R.id.tvBt)
-        tvSqr = findViewById(R.id.tvSqr)
-        tvSqrq = findViewById(R.id.tvSqrq)
-        tvCbbm = findViewById(R.id.tvCbbm)
-        tvJcdd = findViewById(R.id.tvJcdd)
-        tvCbr = findViewById(R.id.tvCbr)
-        tvJdrq = findViewById(R.id.tvJdrq)
-        tvLfkrdw = findViewById(R.id.tvLfkrdw)
-        tvXm = findViewById(R.id.tvXm)
-        tvLfrs = findViewById(R.id.tvLfrs)
-        tvJb = findViewById(R.id.tvJb)
-        tvPtry = findViewById(R.id.tvPtry)
-        tvLfsy = findViewById(R.id.tvLfsy)
-        tvJdje = findViewById(R.id.tvJdje)
-        tvBz = findViewById(R.id.tvBz)
+    fun initViews(context: Context, menu: Menu, spd: Spd,isCreate: Boolean) {
+        LayoutInflater.from(context).inflate(R.layout.header_view_jdsq, this, true)
+        if (isCreate) divider.visibility = View.GONE
+        preparePairs()
+        renderView(menu, spd)
+    }
 
-        // 保存 textView 和 key
+    private fun preparePairs() {
         pairs = ArrayList<PAIR<TextView, String>>().apply {
             add(PAIR(tvSqr, Key.sqr_input))
             add(PAIR(tvSqrq, Key.sqrq_input))
@@ -91,48 +58,14 @@ class JdsqInfoView(context: Context, menu: Menu, spd: Spd) : FrameLayout(context
     private fun renderView(menu: Menu, spd: Spd) {
         tvTitle.text = "${Global.court!!.dmms}${menu.text}"
         tvBt.text = spd.spdXx.bt
-
-        // 遍历展示
-        pairs.forEach {
-            it.apply {
+        pairs.forEach { pair ->
+            pair.apply {
                 textView.text = spd.get(key)
             }
         }
     }
 
-    private fun canEdit(spd: Spd) {
-        val nodeId = spd.spdXx.nodeId
-        if (SpdHelper().canEdit2(nodeId)) {
-            pairs.forEach {
-                it.apply {
-                    textView.isEnabled = true
-                }
-            }
-            tvCbbm.clickDept(Key.cbbmmc_input)
-            tvJcdd.clickCode("就餐地点","JDGL_JDJLSQ_JCDD",Key.jcdd_select)
-            tvCbr.clickDeptUser(Key.textResult, null)
-            tvJdrq.clickDate()
-            tvJb.clickCode("级别","JDGL_JDJLSQ_JB",Key.jb_select)
-            tvPtry.clickDeptUser(Key.textResult, null)
-            RxBus.get().registerEvent(TextResult::class.java, context as LifecycleOwner, Consumer { textResult ->
-                when (textResult.key) {
-                    Key.cbbmmc_input -> tvCbbm
-                    Key.jcdd_select -> tvJcdd
-                    Key.cbr_input -> tvCbr
-                    Key.jdrq_input -> tvJdrq
-                    Key.jb_select -> tvJb
-                    else -> tvPtry
-                }.text = textResult.result
-            })
-        }
-    }
-
     override fun saveToSpd(spd: Spd):Boolean {
-        pairs.forEach {
-            it.apply {
-                spd.set(key, textView.trimText())
-            }
-        }
         return true
     }
 
