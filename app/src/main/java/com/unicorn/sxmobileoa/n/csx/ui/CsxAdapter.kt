1cd6ae6df9c0649a798baca73e11e5a1f055d2fd
ivotai <ivotai@163.com>
2018-11-07 09:58:17 +0800

    超审限

--- app/src/main/java/com/unicorn/sxmobileoa/n/csx/ui/CsxAdapter.kt
+++ app/src/main/java/com/unicorn/sxmobileoa/n/csx/ui/CsxAdapter.kt
@@ -1,15 +1,20 @@
 package com.unicorn.sxmobileoa.n.csx.ui
 
+import android.annotation.SuppressLint
 import com.chad.library.adapter.base.BaseQuickAdapter
-import com.chad.library.adapter.base.BaseViewHolder
 import com.unicorn.sxmobileoa.R
+import com.unicorn.sxmobileoa.app.mess.MyHolder
 import com.unicorn.sxmobileoa.n.csx.model.Csx
-import com.unicorn.sxmobileoa.n.ggxx.model.Ggxx
+import kotlinx.android.synthetic.main.item_csx.*
 
-class CsxAdapter : BaseQuickAdapter<Csx, BaseViewHolder>(R.layout.item_text) {
+class CsxAdapter : BaseQuickAdapter<Csx, MyHolder>(R.layout.item_csx) {
 
-    override fun convert(helper: BaseViewHolder, item: Csx) {
-            helper.setText(R.id.tvText,item.ahqc)
+    @SuppressLint("SetTextI18n")
+    override fun convert(helper: MyHolder, item: Csx) {
+        helper.apply {
+            tvAhqc.text = "${adapterPosition+1}. ${item.ahqc}"
+            tvCsxts.text = "(超期${item.csxts}天)"
+        }
     }
 
 }
