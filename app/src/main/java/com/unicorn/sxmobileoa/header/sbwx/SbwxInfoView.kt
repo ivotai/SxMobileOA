1dcff8dfe7e7b4f41f17e84cdd45ae74a23df95b
ivotai <ivotai@163.com>
2018-11-08 14:06:36 +0800

    设备维修

--- app/src/main/java/com/unicorn/sxmobileoa/header/sbwx/SbwxInfoView.kt
+++ app/src/main/java/com/unicorn/sxmobileoa/header/sbwx/SbwxInfoView.kt
@@ -7,54 +7,32 @@ import android.widget.FrameLayout
 import android.widget.TextView
 import com.unicorn.sxmobileoa.R
 import com.unicorn.sxmobileoa.app.*
-import com.unicorn.sxmobileoa.app.mess.SpdHelper
 import com.unicorn.sxmobileoa.header.BasicInfoView
 import com.unicorn.sxmobileoa.header.PAIR
-import com.unicorn.sxmobileoa.simple.dbxx.model.Dbxx
 import com.unicorn.sxmobileoa.simple.main.model.Menu
 import com.unicorn.sxmobileoa.spd.model.Spd
+import kotlinx.android.extensions.LayoutContainer
+import kotlinx.android.synthetic.main.header_view_sbwx.view.*
 
 @SuppressLint("ViewConstructor")
-class SbwxInfoView(context: Context, menu: Menu, spd: Spd) : FrameLayout(context),
-        BasicInfoView {
+class SbwxInfoView(context: Context, menu: Menu, spd: Spd) : FrameLayout(context), BasicInfoView, LayoutContainer {
+
+    override val containerView = this
+
+    private lateinit var pairs: ArrayList<PAIR<TextView, String>>
 
     init {
         initViews(context, menu, spd)
     }
 
-    lateinit var tvTitle: TextView  // 内部发文标题
-    lateinit var tvBt: TextView     // 真的标题
-
-    lateinit var tvJbdw: TextView   // 不可编辑
-    lateinit var tvJbr: TextView    // 不可编辑
-    lateinit var tvSj: TextView
-    lateinit var tvWxxq: TextView    // 维修详情
-    lateinit var tvWxff: TextView    // 维修方法
-    lateinit var tvWxysjf: TextView    // 预算经费
-    lateinit var tvCljg: TextView    // 预算经费
-
-    private lateinit var pairs: ArrayList<PAIR<TextView, String>>
-
     fun initViews(context: Context, menu: Menu, spd: Spd) {
         LayoutInflater.from(context).inflate(R.layout.header_view_sbwx, this, true)
-        findView()
+        preparePair()
         renderView(menu, spd)
-//        canEdit(dbxx)
+        canEdit(spd)
     }
 
-    private fun findView() {
-        tvTitle = findViewById(R.id.tvTitle)
-        tvBt = findViewById(R.id.tvBt)
-
-        tvJbdw = findViewById(R.id.tvJbdw)
-        tvJbr = findViewById(R.id.tvJbr)
-        tvSj = findViewById(R.id.tvSj)
-        tvWxxq = findViewById(R.id.tvWxxq)
-        tvWxff = findViewById(R.id.tvWxff)
-        tvWxysjf = findViewById(R.id.tvWxysjf)
-        tvCljg = findViewById(R.id.tvCljg)
-
-        // 把 textView 和对应 key 放入 pair
+    private fun preparePair() {
         pairs = ArrayList<PAIR<TextView, String>>().apply {
             add(PAIR(tvJbdw, Key.jbdw_input))
             add(PAIR(tvJbr, Key.jbr_input))
@@ -70,34 +48,22 @@ class SbwxInfoView(context: Context, menu: Menu, spd: Spd) : FrameLayout(context
     private fun renderView(menu: Menu, spd: Spd) {
         tvTitle.text = "${Global.court!!.dmms}${menu.text}"
         tvBt.text = spd.spdXx.bt
-        // 遍历展示
-        pairs.forEach {
-            it.apply {
+        pairs.forEach { pair ->
+            pair.apply {
                 textView.text = spd.get(key)
             }
         }
     }
 
-    private fun canEdit(dbxx: Dbxx) {
-        val nodeId = dbxx.param.nodeId
-        if (SpdHelper().canEdit2(nodeId)) {
-            tvCljg.isEnabled = true
-            return
-        }
-        //
-        val list = listOf(
-                "OA_FLOW_XZZB_SBWX_XXZXYJ",
-                "OA_FLOW_XZZB_SBWX_XXZXBL",
-                "OA_FLOW_XZZB_SBWX_WXRY"
-        )
-        if (nodeId in list) {
-            tvCljg.isEnabled = true
-        }
+    private fun canEdit(spd: Spd) {
+        val nodeId = spd.nodeModel_1!!.nodeid
+        val canEdit = nodeId in listOf("OA_FLOW_XZZB_SBWX_XXZXYJ", "OA_FLOW_XZZB_SBWX_XXZXBL", "OA_FLOW_XZZB_SBWX_WXRY")
+        tvCljg.isEnabled = canEdit
     }
 
-    override fun saveToSpd(spd: Spd) :Boolean{
-        pairs.forEach {
-            it.apply {
+    override fun saveToSpd(spd: Spd): Boolean {
+        pairs.forEach { pair ->
+            pair.apply {
                 spd.set(key, textView.trimText())
             }
         }
