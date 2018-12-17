373e458b350178f57cd5f6a4a740eae75cbec0bd
ivotai <ivotai@163.com>
2018-12-17 11:13:24 +0800

    修改 CourtRequest，fydm 值为 defaultFydm

--- app/src/main/java/com/unicorn/sxmobileoa/simple/court/network/CourtRequest.kt
+++ app/src/main/java/com/unicorn/sxmobileoa/simple/court/network/CourtRequest.kt
@@ -1,13 +1,18 @@
 package com.unicorn.sxmobileoa.simple.court.network
 
-import com.unicorn.sxmobileoa.app.network.model.MaybeRequest
+import com.unicorn.sxmobileoa.app.Global
+import com.unicorn.sxmobileoa.app.Key
+import com.unicorn.sxmobileoa.app.config.ConfigModule
+import com.unicorn.sxmobileoa.app.network.model.request
 
-class CourtRequest : MaybeRequest(busiCode = "fyxx") {
+class CourtRequest : request("fyxx") {
 
     init {
+        addParameter(Key.fydm, (ConfigModule.defaultFydm))
+        Global.loginInfo?.userId?.let { addParameter(Key.userId, it) }
         // 查询所有法院     0
         // 中院和基层法院   2
-         addParameter("type", "2")
+        addParameter("type", "2")
     }
 
 }
